2f0832f1fb4b600ff5edfcbff110a2722fa8b66c
patriciaderieux <6089242+patty345@users.noreply.github.com>
2021-12-26 16:29:37 -0600

    adding the hour blocks

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -20,10 +20,25 @@ $(document).ready(function() {
                 $(this).removeClass("present");
                 $(this).addClass("past");
             }
-            
+            else if (blockTime === timeNow) {
+                $(this).removeClass("past");
+                $(this).removeClass("future");
+                $(this).addClass("present");
+            }
+            else {
+                $(this).removeClass("present");
+                $(this).removeClass("past");
+                $(this).addClass("future");
+            }
         })
     }
 
-
+    $("#hour8 .description").val(localStorage.getItem("hour8"));
+    $("#hour9 .description").val(localStorage.getItem("hour9"));
+    $("#hour10 .description").val(localStorage.getItem("hour10"));
+    $("#hour11 .description").val(localStorage.getItem("hour11"));
+    $("#hour12 .description").val(localStorage.getItem("hour12"));
+    $("#hour13 .description").val(localStorage.getItem("hour13"));
+    $("#hour14 .description").val(localStorage.getItem("hour14"));
 
  })
