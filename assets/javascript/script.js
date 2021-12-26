191a1682c16b20a9bdd0ea29a1d943ab38038327
patriciaderieux <6089242+patty345@users.noreply.github.com>
2021-12-26 16:01:51 -0600

    added local storage

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -2,5 +2,15 @@ var todayDate = moment().format("dddd, MM Do YYYY");
 $("#currentDay").html(todayDate);
 
 $(document).ready(function() {
+    $(".saveBtn").on("click", function () {
+        var text = $(this).siblings(".description").val();
+        var time = $(this).parent().attr("id");
+
+        localStorage.setItem(time, text);
+    })
+
     
-} )
+
+
+
+ })
